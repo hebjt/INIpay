f42617a233418eefc1adf2ae986ccaa58da325ee
hebjt <cuijingtao@mint>
2013-05-17 17:54:09 +0800

    first

---
+++ src/com/tanghsk/inipay/action/INIpayAction.java
@@ -0,0 +1,10 @@
+package com.tanghsk.inipay.action;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/inipay/**")
+public class INIpayAction {
+
+}
